bac10139a3d6192b80ffe123d36f7c2f6f87bc28
juandgo <juandavidgo1997@gmail.com>
2025-07-21 17:25:57 -0500

    Cambios a mi repo add

--- src/ContactosAPI/Program.cs
+++ src/ContactosAPI/Program.cs
@@ -43,6 +43,6 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
-//comentario para actualizar mi repositorio
+//comentario para actualizar mi repositorio added
 
 public partial class Program(){}
